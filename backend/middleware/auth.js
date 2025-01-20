8c925bc1ad1b6d45b8f2d918b84867c47bdde69c
Hector-John <hectorjohn254@gmail.com>
2025-01-20 22:22:45 +0300

    fixed cart refresh issue

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,16 +2,16 @@ import jwt from 'jsonwebtoken';
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1]; // Extract token from Authorization header
+    const token = req.headers; 
     if (!token) {
-      return res.status(401).json({ success: false, message: 'Not authorized, login again' });
+      return res.status(401).json({ success: false, message: 'Not authorized, login' });
     }
-    const decodedToken = jwt.verify(token, process.env.jwtSecret);
-    req.userId = decodedToken.id; 
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = decodedToken.id; 
     next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ success: false, message: 'Not authorized, login again' });
+    return res.status(401).json({ success: false, message: 'Error'});
   }
 };
 
--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { addCart, removeCart, getCart } from '../controllers/cartController.js';
 import authMiddleware from '../middleware/auth.js';
 
-const cartRouter = express.Router();
+const cartRouter = express.Router(); 
 
 cartRouter.post('/add', authMiddleware, addCart);
 cartRouter.post('/remove', authMiddleware, removeCart);
